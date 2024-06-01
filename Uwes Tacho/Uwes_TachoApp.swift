0a5d445bb0d0381717fcf0103c12dc6561e1a19d
UPetersen <uwe.n.petersen@icloud.com>
2024-06-01 13:32:51 +0200

    Initial Commit

---
+++ Uwes Tacho/Uwes_TachoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uwes_TachoApp.swift
+//  Uwes Tacho
+//
+//  Created by Uwe Petersen on 01.06.24.
+//
+
+import SwiftUI
+
+@main
+struct Uwes_TachoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
